b6370b300ad5a18bcf735328a55bdb7afba24a8b
escoffon <escoffon>
1998-07-06 14:37:54 +0000

    Exported {TclProc}CleanupProc, {Tcl}ProcDeleteProc, and {TclProc}InterpProc, for use by the TclPro compiler.

--- generic/tclProc.c
+++ generic/tclProc.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * SCCS: %Z% $Id$ 
+ * SCCS: @(#) tclProc.c 1.116 97/10/29 18:33:24
  */
 
 #include "tclInt.h"
