834d9112547c747fc2e15c8459398218ea78d33f
stanton <stanton>
1998-10-06 03:09:26 +0000

    merge 8.0.4 into 8.1

--- win/rmd.bat
+++ win/rmd.bat
@@ -1,13 +1,18 @@
 @echo off
 rem RCS: @(#) $Id$
 
-if not exist %1 goto end
+if not exist %1\tag.txt goto end
 
-if %OS% == Windows_NT goto winnt
+echo Removing directory %1
 
-echo Add support for Win 95 please
-goto end
+if "%OS%" == "Windows_NT" goto winnt
 
+cd %1
+if errorlevel 1 goto end
+del *.*
+cd ..
+rmdir %1
+if errorlevel 1 goto end
 goto success
 
 :winnt
