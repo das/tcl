dea9fe34cf5a669b4a9d121184c686450b12ed01
rjohnson <rjohnson>
1998-10-13 20:30:21 +0000

    Fixed bug in Tcl_SetListObj - it used to create invalid Tcl_Obj if you passed in 0 elements.  Despite what docs said.
    Also updated a few copyright notices.

--- generic/tcl.h
+++ generic/tcl.h
@@ -7,6 +7,7 @@
  * Copyright (c) 1987-1994 The Regents of the University of California.
  * Copyright (c) 1994-1997 Sun Microsystems, Inc.
  * Copyright (c) 1993-1996 Lucent Technologies.
+ * Copyright (c) 1998 by Scriptics Corporation.
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
--- generic/tclListObj.c
+++ generic/tclListObj.c
@@ -5,6 +5,7 @@
  *	type.
  *
  * Copyright (c) 1995-1997 Sun Microsystems, Inc.
+ * Copyright (c) 1998 by Scriptics Corporation.
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
@@ -260,6 +261,8 @@ Tcl_SetListObj(objPtr, objc, objv)
 	
 	objPtr->internalRep.otherValuePtr = (VOID *) listRepPtr;
 	objPtr->typePtr = &tclListType;
+    } else {
+	objPtr->bytes = tclEmptyStringRep;
     }
 }
 
