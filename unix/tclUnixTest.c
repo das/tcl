1c246cc980883c653419986c2ec10fc3d5344631
rjohnson <rjohnson>
1998-10-14 00:32:55 +0000

    Made some the prototypes & function defs consistant in tclUnixTest.c

--- unix/tclUnixTest.c
+++ unix/tclUnixTest.c
@@ -4,6 +4,7 @@
  *	Contains platform specific test commands for the Unix platform.
  *
  * Copyright (c) 1996 Sun Microsystems, Inc.
+ * Copyright (c) 1998 by Scriptics Corporation.
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
@@ -516,7 +517,7 @@ TestgetopenfileCmd(clientData, interp, argc, argv)
  *----------------------------------------------------------------------
  */
 
-int
+static int
 TestalarmCmd(clientData, interp, argc, argv)
     ClientData clientData;		/* Not used. */
     Tcl_Interp *interp;			/* Current interpreter. */
@@ -593,7 +594,7 @@ AlarmHandler()
  *----------------------------------------------------------------------
  */
 
-int
+static int
 TestgotsigCmd(clientData, interp, argc, argv)
     ClientData clientData;		/* Not used. */
     Tcl_Interp *interp;			/* Current interpreter. */
