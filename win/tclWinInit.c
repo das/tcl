226f9ddc4fb6841342c842eae61067327322dd79
suresh <suresh>
1998-10-20 17:27:43 +0000

    On Windows, Added the "debug" element of the tcl_platform array which indicates that the particular Tcl shell has been compiled with debug information Using "info exists tcl_platform(debug)" a Tcl script can direct the interpreter to load debug versions of DLLs with the load command.

--- win/tclWinInit.c
+++ win/tclWinInit.c
@@ -240,6 +240,18 @@ TclPlatformInit(interp)
 		TCL_GLOBAL_ONLY);
     }
 
+#ifdef _DEBUG
+    /*
+     * The existence of the "debug" element of the tcl_platform array indicates
+     * that this particular Tcl shell has been compiled with debug information.
+     * Using "info exists tcl_platform(debug)" a Tcl script can direct the 
+     * interpreter to load debug versions of DLLs with the load command.
+     */
+
+    Tcl_SetVar2(interp, "tcl_platform", "debug", "1",
+		TCL_GLOBAL_ONLY);
+#endif
+
     /*
      * Set up the HOME environment variable from the HOMEDRIVE & HOMEPATH
      * environment variables, if necessary.
