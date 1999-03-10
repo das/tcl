af729fd43cffa197ca3b373c7f72f975d830dd4c
redman <redman>
1999-03-10 23:44:19 +0000

    Fixed declarations of empty slots.

--- generic/tclStubInit.c
+++ generic/tclStubInit.c
@@ -349,12 +349,12 @@ TclStubs tclStubs = {
     Tcl_WaitPid, /* 277 */
     panicVA, /* 278 */
     Tcl_GetVersion, /* 279 */
-    use, /* 280 */
-    use, /* 281 */
-    use, /* 282 */
-    use, /* 283 */
-    use, /* 284 */
-    use, /* 285 */
+    NULL, /* 280 */
+    NULL, /* 281 */
+    NULL, /* 282 */
+    NULL, /* 283 */
+    NULL, /* 284 */
+    NULL, /* 285 */
     Tcl_AppendObjToObj, /* 286 */
     Tcl_AlertNotifier, /* 287 */
     Tcl_CreateEncoding, /* 288 */
