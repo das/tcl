ff210764b4c1d4051aa2aa6362a672c4aa96e9d2
hershey <hershey>
1999-04-07 01:59:27 +0000

    change tcltest namespace variables from matchingTests and skippingTests to match and skip.
    removed nonexistent constraint from interp.test and renumbered tests to
    remove duplicates.

--- tests/all.tcl
+++ tests/all.tcl
@@ -16,11 +16,11 @@ set ::tcltest::testSingleFile false
 
 puts stdout "Tcl $tcl_patchLevel tests running in interp:  [info nameofexecutable]"
 puts stdout "Tests running in working dir:  $::tcltest::workingDir"
-if {[llength $::tcltest::skippingTests] > 0} {
-    puts stdout "Skipping tests that match:  $::tcltest::skippingTests"
+if {[llength $::tcltest::skip] > 0} {
+    puts stdout "Skipping tests that match:  $::tcltest::skip"
 }
-if {[llength $::tcltest::matchingTests] > 0} {
-    puts stdout "Only running tests that match:  $::tcltest::matchingTests"
+if {[llength $::tcltest::match] > 0} {
+    puts stdout "Only running tests that match:  $::tcltest::match"
 }
 
 # Use command line specified glob pattern (specified by -file or -f)
