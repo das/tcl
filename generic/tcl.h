1a677c3ee9358855b0002082c178b438e106bcb4
stanton <stanton>
1999-04-30 23:35:40 +0000

    * Changed version to 8.1.1.

--- generic/tcl.h
+++ generic/tcl.h
@@ -45,10 +45,10 @@
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   1
 #define TCL_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TCL_RELEASE_SERIAL  0
+#define TCL_RELEASE_SERIAL  1
 
 #define TCL_VERSION	    "8.1"
-#define TCL_PATCH_LEVEL	    "8.1.0"
+#define TCL_PATCH_LEVEL	    "8.1.1"
 
 /*
  * The following definitions set up the proper options for Windows
