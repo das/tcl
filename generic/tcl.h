d8a76897ce87999b0f8b61acb683395f0524a121
hobbs <hobbs>
1999-09-24 02:01:41 +0000

    1999-09-23  Jeff Hobbs  <hobbs@scriptics.com>
    	* README:
    	* generic/tcl.h:
    	* tools/tcl.wse.in: updated text to reflect 8.2.1 where necessary

--- generic/tcl.h
+++ generic/tcl.h
@@ -56,10 +56,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   2
 #define TCL_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TCL_RELEASE_SERIAL  0
+#define TCL_RELEASE_SERIAL  1
 
 #define TCL_VERSION	    "8.2"
-#define TCL_PATCH_LEVEL	    "8.2.0"
+#define TCL_PATCH_LEVEL	    "8.2.1"
 
 /*
  * The following definitions set up the proper options for Windows
