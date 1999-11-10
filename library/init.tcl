4dc695f2e4061bdca00025768fda5b4b23920b87
hobbs <hobbs>
1999-11-10 02:52:10 +0000

    * library/init.tcl: removed the installed binary directory from 	the auto_path variable

--- library/init.tcl
+++ library/init.tcl
@@ -52,12 +52,10 @@ if {[string compare [info library] {}]} {
 	}
     }
 }
-foreach __dir [list [file dirname [info nameofexecutable]] \
-	[file join [file dirname [file dirname \
-	[info nameofexecutable]]] lib]] {
-    if {[lsearch -exact $auto_path $__dir] < 0} {
-	lappend auto_path $__dir
-    }
+set __dir [file join [file dirname [file dirname \
+	[info nameofexecutable]]] lib]
+if {[lsearch -exact $auto_path $__dir] < 0} {
+    lappend auto_path $__dir
 }
 if {[info exist tcl_pkgPath]} {
     foreach __dir $tcl_pkgPath {
