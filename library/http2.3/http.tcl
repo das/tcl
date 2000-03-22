46a72258ea2182738597ba76b41ae8225af3387a
sandeep <sandeep>
2000-03-22 21:18:24 +0000

    2000-03-22  Sandeep Tamhankar <sandeep@scriptics.com>
            * library/http2.1/http.tcl: Fixed bug 4463, where we were getting
            a stack trace if we tried to publish a project to a good host but
            a port where there was no server listening.  It turned out the
            problem was a stray fileevent that needed to be cleared.  Also,
    	fixed a bug where http::code could stack trace if called on a bad
    	token (one which didn't represent a successful geturl) by adding
    	an http element to the state array in geturl.

--- library/http2.3/http.tcl
+++ library/http2.3/http.tcl
@@ -227,6 +227,7 @@ proc http::geturl { url args } {
         type            text/html
         body            {}
 	status		""
+	http            ""
     }
     set options {-blocksize -channel -command -handler -headers \
 	    -progress -query -queryblocksize -querychannel -queryprogress\
@@ -331,10 +332,10 @@ proc http::geturl { url args } {
     if {$state(-timeout) > 0} {
 	fileevent $s writable [list http::Connect $token]
 	http::wait $token
+	fileevent $s writable {}
 	if {![string equal $state(status) "connect"]} {
 	    return $token
 	}
-	fileevent $s writable {}
 	set state(status) ""
     }
 
