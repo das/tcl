f242451be7380e7c47186179e810ffa6ddfee155
jenn <jenn>
2000-10-24 22:30:17 +0000

    * tests/all.tcl: Removed support for tcltest 1.0.
    * tests/tcltest.test:
    * library/tcltest1.0/tcltest.tcl:
    * library/tcltest1.0/pkgIndex.tcl:
    * docs/tcltest.n: Moved tcltest2 code so that it's the standard
    version of tcltest.  Removed all tcltest2 files
    (tests/tcltest2.test, library/tcltest1.0/tcltest2.tcl,
    docs/tcltest2.n).

--- tests/all.tcl
+++ tests/all.tcl
@@ -13,48 +13,7 @@
 set tcltestVersion [package require tcltest]
 namespace import -force tcltest::*
 
-if {[package vcompare $tcltestVersion 1.0]} {
-    tcltest::testsDirectory [file dir [info script]]
-    tcltest::runAllTests
-} else {
-    set ::tcltest::testSingleFile false
-    set ::tcltest::testsDirectory [file dir [info script]]
-
-    # We need to ensure that the testsDirectory is absolute
-    ::tcltest::normalizePath ::tcltest::testsDirectory
-
-    puts stdout "Tcl $tcl_patchLevel tests running in interp:  [info nameofexecutabl
-    e]"
-    puts stdout "Tests running in working dir:  $::tcltest::testsDirectory"
-    if {[llength $::tcltest::skip] > 0} {
-	puts stdout "Skipping tests that match:  $::tcltest::skip"
-    }
-    if {[llength $::tcltest::match] > 0} {
-	puts stdout "Only running tests that match:  $::tcltest::match"
-    }
-
-    if {[llength $::tcltest::skipFiles] > 0} {
-	puts stdout "Skipping test files that match:  $::tcltest::skipFiles"
-    }
-    if {[llength $::tcltest::matchFiles] > 0} {
-	puts stdout "Only sourcing test files that match:  $::tcltest::matchFiles"
-    }
-
-    set timeCmd {clock format [clock seconds]}
-    puts stdout "Tests began at [eval $timeCmd]"
-
-    # source each of the specified tests
-    foreach file [lsort [::tcltest::getMatchingFiles]] {
-	set tail [file tail $file]
-	puts stdout $tail
-	if {[catch {source $file} msg]} {
-	    puts stdout $msg
-	}
-    }
-
-    # cleanup
-    puts stdout "\nTests ended at [eval $timeCmd]"
-    ::tcltest::cleanupTests 1
-}
+tcltest::testsDirectory [file dir [info script]]
+tcltest::runAllTests
 
 return
