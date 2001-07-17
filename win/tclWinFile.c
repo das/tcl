cf3a796506676f08ca37932c707db3d06aa98052
mdejong <mdejong>
2001-07-17 19:40:37 +0000

    * win/tclWinFile.c (TclpReadlink): Add Cygwin specific definition 	for the TclpReadlink function. This method implements reading of 	symbolic links when build with Cygwin.

--- win/tclWinFile.c
+++ win/tclWinFile.c
@@ -689,6 +689,51 @@ TclpChdir(path)
     return 0;
 }
 
+#ifdef __CYGWIN__
+/*
+ *---------------------------------------------------------------------------
+ *
+ * TclpReadlink --
+ *
+ *     This function replaces the library version of readlink().
+ *
+ * Results:
+ *     The result is a pointer to a string specifying the contents
+ *     of the symbolic link given by 'path', or NULL if the symbolic
+ *     link could not be read.  Storage for the result string is
+ *     allocated in bufferPtr; the caller must call Tcl_DStringFree()
+ *     when the result is no longer needed.
+ *
+ * Side effects:
+ *     See readlink() documentation.
+ *
+ *---------------------------------------------------------------------------
+ */
+
+char *
+TclpReadlink(path, linkPtr)
+    CONST char *path;          /* Path of file to readlink (UTF-8). */
+    Tcl_DString *linkPtr;      /* Uninitialized or free DString filled
+                                * with contents of link (UTF-8). */
+{
+    char link[MAXPATHLEN];
+    int length;
+    char *native;
+    Tcl_DString ds;
+
+    native = Tcl_UtfToExternalDString(NULL, path, -1, &ds);
+    length = readlink(native, link, sizeof(link));     /* INTL: Native. */
+    Tcl_DStringFree(&ds);
+    
+    if (length < 0) {
+       return NULL;
+    }
+
+    Tcl_ExternalToUtfDString(NULL, link, length, linkPtr);
+    return Tcl_DStringValue(linkPtr);
+}
+#endif /* __CYGWIN__ */
+
 /*
  *----------------------------------------------------------------------
  *
