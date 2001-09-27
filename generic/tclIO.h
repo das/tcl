7c341b358628f9a901c668a3bb8ba3a16111a614
dkf <dkf>
2001-09-27 14:25:42 +0000

    Backing out unwise changes

--- generic/tclIO.h
+++ generic/tclIO.h
@@ -367,12 +367,12 @@ typedef struct GetsState {
     Tcl_EncodingState state;	/* The encoding state just before the last
 				 * external to UTF-8 conversion in
 				 * FilterInputBytes(). */
-    Tcl_Length rawRead;		/* The number of bytes removed from bufPtr
+    int rawRead;		/* The number of bytes removed from bufPtr
 				 * in the last call to FilterInputBytes(). */
-    Tcl_Length bytesWrote;	/* The number of bytes of UTF-8 data
+    int bytesWrote;		/* The number of bytes of UTF-8 data
 				 * appended to objPtr during the last call to
 				 * FilterInputBytes(). */
-    Tcl_Length charsWrote;	/* The corresponding number of UTF-8
+    int charsWrote;		/* The corresponding number of UTF-8
 				 * characters appended to objPtr during the
 				 * last call to FilterInputBytes(). */
     int totalChars;		/* The total number of UTF-8 characters
