9361be2240b53cb74bedb728a4c5f331763f76fe
hobbs <hobbs>
2001-10-16 21:13:50 +0000

    * unix/tclLoadAout.c (TclGuessPackageName): removed unused vars 	and fixed warnings. [Bug #446622] (lim)

--- unix/tclLoadAout.c
+++ unix/tclLoadAout.c
@@ -22,6 +22,11 @@
 #ifdef HAVE_EXEC_AOUT_H
 #   include <sys/exec_aout.h>
 #endif
+#ifdef HAVE_UNISTD_H
+#   include <unistd.h>
+#else
+#   include "../compat/unistd.h"
+#endif
 
 /*
  * Some systems describe the a.out header in sys/exec.h, and some in
@@ -336,7 +341,7 @@ FindLibraries (interp, fileName, buf)
      Tcl_DString * buf;		/* Buffer where the -l an -L flags */
 {
   FILE * f;			/* The load module */
-  int c;			/* Byte from the load module */
+  int c = 0;			/* Byte from the load module */
   char * p;
   Tcl_DString ds;
   CONST char *native;
@@ -475,9 +480,8 @@ TclGuessPackageName(fileName, bufPtr)
 				 * package name to this if possible. */
 {
     char *p, *q, *r;
-    int srcOff, dstOff;
 
-    if (q = strrchr(fileName,'/')) {
+    if ((q = strrchr(fileName,'/'))) {
 	q++;
     } else {
 	q = fileName;
