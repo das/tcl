eda4995ff64704f55fe89a4970023171006d622f
dgp <dgp>
2002-09-27 18:04:11 +0000

    * README:               Bumped to version 8.4.1 to avoid confusion         * generic/tcl.h:        of CVS snapshots with the actual 8.4.0         * tools/tcl.wse.in:     release.         * unix/configure.in:         * unix/tcl.spec:         * win/configure.in:
            * unix/configure:       autoconf
            * win/configure:

--- generic/tcl.h
+++ generic/tcl.h
@@ -57,10 +57,10 @@ extern "C" {
 #define TCL_MAJOR_VERSION   8
 #define TCL_MINOR_VERSION   4
 #define TCL_RELEASE_LEVEL   TCL_FINAL_RELEASE
-#define TCL_RELEASE_SERIAL  0
+#define TCL_RELEASE_SERIAL  1
 
 #define TCL_VERSION	    "8.4"
-#define TCL_PATCH_LEVEL	    "8.4.0"
+#define TCL_PATCH_LEVEL	    "8.4.1"
 
 /*
  * The following definitions set up the proper options for Windows
