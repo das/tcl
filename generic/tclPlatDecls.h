20d5aa38d9b6950fcba2dc86595323849600b584
hobbs <hobbs>
2002-10-15 20:23:18 +0000

    * generic/tclPlatDecls.h (tchar.h): Do not include on Cygwin.

--- generic/tclPlatDecls.h
+++ generic/tclPlatDecls.h
@@ -13,12 +13,21 @@
 #define _TCLPLATDECLS
 
 /*
- *  Pull in the definition of TCHAR.  Hopefully the compile flags
- *  of the core are matching against your project build for these
- *  public functions.  BE AWARE.
+ *  Pull in the typedef of TCHAR for windows.
  */
-#if defined(__WIN32__) && !defined(_INC_TCHAR)
-#include <tchar.h>
+#if defined(__CYGWIN__)
+    typedef char TCHAR;
+#elif defined(__WIN32__) && !defined(_TCHAR_DEFINED)
+#   include <tchar.h>
+#   ifndef _TCHAR_DEFINED
+	/* Borland seems to forget to set this. */
+        typedef _TCHAR TCHAR;
+#	define _TCHAR_DEFINED
+#   endif
+#   if defined(_MSC_VER) && defined(__STDC__)
+	/* MSVC++ misses this. */
+	typedef _TCHAR TCHAR;
+#   endif
 #endif
 
 /* !BEGIN!: Do not edit below this line. */
