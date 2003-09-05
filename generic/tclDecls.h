aca706a40b3ab46a0497a7a8558aaa89b8ffcbb1
dgp <dgp>
2003-09-05 23:10:04 +0000

    stupid CVS conflicts...

--- generic/tclDecls.h
+++ generic/tclDecls.h
@@ -8,11 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
-<<<<<<< tclDecls.h
  * RCS: @(#) $Id$
-=======
- * RCS: @(#) $Id$
->>>>>>> 1.99
  */
 
 #ifndef _TCLDECLS
--- generic/tclStubInit.c
+++ generic/tclStubInit.c
@@ -8,11 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
-<<<<<<< tclStubInit.c
  * RCS: @(#) $Id$
-=======
- * RCS: @(#) $Id$
->>>>>>> 1.88
  */
 
 #include "tclInt.h"
