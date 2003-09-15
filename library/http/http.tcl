bc557f3ca435f0a26bacc04b81a3aa43a64d4409
patthoyts <patthoyts>
2003-09-15 14:24:50 +0000

    Fixed comment typos.

--- library/http/http.tcl
+++ library/http/http.tcl
@@ -66,7 +66,7 @@ namespace eval http {
 
 # http::register --
 #
-#     See documentaion for details.
+#     See documentation for details.
 #
 # Arguments:
 #     proto           URL protocol prefix, e.g. https
@@ -101,7 +101,7 @@ proc http::unregister {proto} {
 
 # http::config --
 #
-#	See documentaion for details.
+#	See documentation for details.
 #
 # Arguments:
 #	args		Options parsed by the procedure.
@@ -180,7 +180,7 @@ proc http::Finish { token {errormsg ""} {skipCB 0}} {
 
 # http::reset --
 #
-#	See documentaion for details.
+#	See documentation for details.
 #
 # Arguments:
 #	token	Connection token.
@@ -824,7 +824,7 @@ proc http::Eof {token} {
 
 # http::wait --
 #
-#	See documentaion for details.
+#	See documentation for details.
 #
 # Arguments:
 #	token	Connection token.
@@ -846,7 +846,7 @@ proc http::wait {token} {
 
 # http::formatQuery --
 #
-#	See documentaion for details.
+#	See documentation for details.
 #	Call http::formatQuery with an even number of arguments, where 
 #	the first is a name, the second is a value, the third is another 
 #	name, and so on.
