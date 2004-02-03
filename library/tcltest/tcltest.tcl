6b9a9598dd8fa8a6a3b0b7bc0d5412a1bb88d205
dgp <dgp>
2004-02-03 18:49:13 +0000

    * library/tcltest/tcltest.tcl:  Corrected parsing of single         command line argument (option with missing value) [Bug 833910]         * library/tcltest/pkgIndex.tcl: Bump to version 2.2.5.

--- library/tcltest/tcltest.tcl
+++ library/tcltest/tcltest.tcl
@@ -24,7 +24,7 @@ namespace eval tcltest {
     # When the version number changes, be sure to update the pkgIndex.tcl file,
     # and the install directory in the Makefiles.  When the minor version
     # changes (new feature) be sure to update the man page as well.
-    variable Version 2.2.4
+    variable Version 2.2.5
 
     # Compatibility support for dumb variables defined in tcltest 1
     # Do not use these.  Call [package provide Tcl] and [info patchlevel]
@@ -1419,7 +1419,7 @@ proc tcltest::ProcessFlags {flagArray} {
 	RemoveAutoConfigureTraces
     } else {
 	set args $flagArray
-	while {[llength $args] && [catch {eval configure $args} msg]} {
+	while {[llength $args]>1 && [catch {eval configure $args} msg]} {
 
 	    # Something went wrong parsing $args for tcltest options
 	    # Check whether the problem is "unknown option"
@@ -1449,6 +1449,11 @@ proc tcltest::ProcessFlags {flagArray} {
 		set args [lrange $args 2 end]
 	    }
 	    set args [lrange $args 2 end]
+	}
+	if {[llength $args] == 1} {
+	    puts [errorChannel] \
+		    "missing value for option [lindex $args 0]"
+	    exit 1
 	}
     }
 
