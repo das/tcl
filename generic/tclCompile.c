ce160c26aae072cdfd99f565eaad21eb4f7803c0
msofer <msofer>
2004-03-29 02:17:58 +0000

    * generic/tclCompile.c (TclCompileScript): corrected possible 	segfault when a compilation returns TCL_OUTLINE_COMPILE after 	having grown the compile environment [Bug 925121].

--- generic/tclCompile.c
+++ generic/tclCompile.c
@@ -990,7 +990,8 @@ TclCompileScript(interp, script, numBytes, nested, envPtr)
 			        && !(cmdPtr->flags & CMD_HAS_EXEC_TRACES)
 			        && !(iPtr->flags & DONT_COMPILE_CMDS_INLINE)) {
 			    int savedNumCmds = envPtr->numCommands;
-			    unsigned char *savedCodeNext = envPtr->codeNext;
+			    unsigned int savedCodeNext =
+				    envPtr->codeNext - envPtr->codeStart;
 
 			    code = (*(cmdPtr->compileProc))(interp, &parse,
 			            envPtr);
@@ -1003,7 +1004,7 @@ TclCompileScript(interp, script, numBytes, nested, envPtr)
 				 * TCL_OUT_LINE_COMPILE [Bugs 705406 and 735055]
 				 */
 				envPtr->numCommands = savedNumCmds;
-				envPtr->codeNext = savedCodeNext;
+				envPtr->codeNext = envPtr->codeStart + savedCodeNext;
 			    } else { /* an error */
 				/*
 				 * There was a compilation error, the last
