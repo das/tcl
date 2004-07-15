7b9cc273f29eb5515a6128592aa6afc4444b85c7
vasiljevic <vasiljevic>
2004-07-15 09:57:10 +0000

    Remoevd initialization of TSD for the new thread in NewThreadProc() since this will result in TclInitNotifier never being called.

--- generic/tclEvent.c
+++ generic/tclEvent.c
@@ -1185,19 +1185,10 @@ NewThreadProc(ClientData clientData)
     ClientData threadClientData;
     Tcl_ThreadCreateProc *threadProc;
 
-    TCL_TSD_INIT(&dataKey);
-
     cdPtr = (ThreadClientData*)clientData;
     threadProc = cdPtr->proc;
     threadClientData = cdPtr->clientData;
     Tcl_Free((char*)clientData); /* Allocated in Tcl_CreateThread() */
-
-    /*
-     * Please see the SF Bug #770053 comments to find out why
-     * this one below is commented out.
-     */
-
-    /* TclInitNotifier(); */
 
     (*threadProc)(threadClientData);
 
