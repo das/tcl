6eae5dcf986fd0e70aa39f060329484b01f7ab34
das <das>
2004-07-20 10:12:29 +0000

    * unix/tclUnixFile.c (NativeMatchType): added support for 	readonly matching of user immutable files (where available).

--- unix/tclUnixFile.c
+++ unix/tclUnixFile.c
@@ -378,8 +378,12 @@ NativeMatchType(
 	    /* 
 	     * readonly means that there are NO write permissions
 	     * (even for user), but execute is OK for anybody
+	     * OR that the user immutable flag is set (where supported).
 	     */
 	    if (((types->perm & TCL_GLOB_PERM_RONLY) &&
+#if defined(HAVE_CHFLAGS) && defined(UF_IMMUTABLE)
+			!(buf.st_flags & UF_IMMUTABLE) &&
+#endif
 			(buf.st_mode & (S_IWOTH|S_IWGRP|S_IWUSR))) ||
 		((types->perm & TCL_GLOB_PERM_R) &&
 			(access(nativeEntry, R_OK) != 0)) ||
