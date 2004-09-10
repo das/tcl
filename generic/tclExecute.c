7c15cfbef193cc04bb3e22f0fc9485553ca370ff
msofer <msofer>
2004-09-10 15:30:01 +0000

    * generic/tclExecute.c (INST_CONCAT1): added a peephole 	optimisation for concatting an empty string. This enables 	replacing the idiom 'K $x [set x {}]' by '$x[set x {}]' for 	fastest execution.

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -1264,6 +1264,20 @@ TclExecuteByteCode(interp, codePtr)
 	    int totalLen = 0;
 	    
 	    /*
+	     * Peephole optimisation for appending an empty string.
+	     * This enables replacing 'K $x [set x{}]' by '$x[set x{}]'
+	     * for fastest execution.
+	     */
+
+	    if (opnd == 2) {
+		Tcl_GetStringFromObj(stackPtr[stackTop], &length);
+		if (length == 0) {
+		    /* Just drop the top item from the stack */
+		    NEXT_INST_F(2, 1, 0);
+		}
+	    }
+
+	    /*
 	     * Concatenate strings (with no separators) from the top
 	     * opnd items on the stack starting with the deepest item.
 	     * First, determine how many characters are needed.
