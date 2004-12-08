aa9ec3912e99a61a3e0c01e21f71de58077d38b8
kennykb <kennykb>
2004-12-08 20:24:55 +0000

    changed Win configurator to use SIZEOF macros for Tcl_WideInt

--- compat/strtoll.c
+++ compat/strtoll.c
@@ -38,11 +38,7 @@
  *----------------------------------------------------------------------
  */
 
-#if TCL_WIDE_INT_IS_LONG
-long long
-#else
 Tcl_WideInt
-#endif
 strtoll(string, endPtr, base)
     CONST char *string;		/* String of ASCII digits, possibly
 				 * preceded by white space.  For bases
