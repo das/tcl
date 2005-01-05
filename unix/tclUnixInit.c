47d0c6a1f870ac68a07196b0b2330fe404c6fbd7
dkf <dkf>
2005-01-05 22:14:39 +0000

    Add more locale mapping info for Chinese. [1084595]

--- unix/tclUnixInit.c
+++ unix/tclUnixInit.c
@@ -136,6 +136,11 @@ static CONST LocaleTable localeTable[] = {
     {"ru_SU",		"iso8859-5"},		
 
     {"zh",		"cp936"},
+    {"zh_CN.gb2312",	"euc-cn"},
+    {"zh_CN.GB2312",	"euc-cn"},
+    {"zh_CN.GBK",	"euc-cn"},
+    {"zh_TW.Big5",	"big5"},
+    {"zh_TW",		"euc-tw"},
 
     {NULL, NULL}
 };
