9e19d2d108439360dbc6d8d4851b8a0f5ae9b9a1
dgp <dgp>
2005-02-01 17:27:26 +0000

    * generic/tclExecute.c (TclCompEvalObj): Removed stray statement         left behind in prior code reorganization.

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -1008,7 +1008,6 @@ TclCompEvalObj(interp, objPtr)
 	    iPtr->numLevels--;
 	    return result;
 	}
-	iPtr->evalFlags = 0;
 	codePtr = (ByteCode *) objPtr->internalRep.otherValuePtr;
     } else {
 	/*
