a10c7b7c285fa57ba33260c93259e14fb66114f8
msofer <msofer>
2005-04-25 10:05:25 +0000

    fix comments

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -4713,7 +4713,10 @@ TclExecuteByteCode(interp, codePtr)
 	     * If some var in some var list still has a remaining list
 	     * element iterate one more time. Assign to var the next
 	     * element from its value list. We already checked above
-	     * that each list temp holds a valid list object.
+	     * that each list temp holds a valid list object (by calling
+	     * Tcl_ListObjLength), but cannot rely on that check remaining
+	     * valid: one list could have been shimmered as a side effect of
+	     * setting a traced variable.
 	     */
 		
 	    if (continueLoop) {
