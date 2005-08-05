2defbc44043a86f4186e8feacc85f0dbd3e72a40
dkf <dkf>
2005-08-05 23:57:35 +0000

    regen

--- generic/tclStubInit.c
+++ generic/tclStubInit.c
@@ -268,19 +268,19 @@ TclIntStubs tclIntStubs = {
     NULL, /* 181 */
     TclpLocaltime, /* 182 */
     TclpGmtime, /* 183 */
-    TclThreadStorageLockInit, /* 184 */
-    TclThreadStorageLock, /* 185 */
-    TclThreadStorageUnlock, /* 186 */
-    TclThreadStoragePrint, /* 187 */
-    TclThreadStorageGetHashTable, /* 188 */
-    TclThreadStorageInit, /* 189 */
-    TclThreadStorageDataKeyInit, /* 190 */
-    TclThreadStorageDataKeyGet, /* 191 */
-    TclThreadStorageDataKeySet, /* 192 */
-    TclFinalizeThreadStorageThread, /* 193 */
-    TclFinalizeThreadStorage, /* 194 */
-    TclFinalizeThreadStorageData, /* 195 */
-    TclFinalizeThreadStorageDataKey, /* 196 */
+    NULL, /* 184 */
+    NULL, /* 185 */
+    NULL, /* 186 */
+    NULL, /* 187 */
+    NULL, /* 188 */
+    NULL, /* 189 */
+    NULL, /* 190 */
+    NULL, /* 191 */
+    NULL, /* 192 */
+    NULL, /* 193 */
+    NULL, /* 194 */
+    NULL, /* 195 */
+    NULL, /* 196 */
     TclCompEvalObj, /* 197 */
     TclObjGetFrame, /* 198 */
     NULL, /* 199 */
