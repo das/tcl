6f0d515e474f26e7f67be99ba240c043a2dfe1ee
kennykb <kennykb>
2005-08-24 18:56:32 +0000

    corrected TRACE bugs, overflowed negative #s, and bad test constraints

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -2673,7 +2673,7 @@ TclExecuteByteCode(interp, codePtr)
 	result = Tcl_GetBooleanFromObj(NULL, value2Ptr, &i2);
 	if (result != TCL_OK) {
 	    TRACE(("\"%.20s\" => ILLEGAL TYPE %s \n", O2S(value2Ptr),
-		    (value2Ptr->typePtr? valuewPtr->typePtr->name : "null")));
+		    (value2Ptr->typePtr? value2Ptr->typePtr->name : "null")));
 	    IllegalExprOperandType(interp, pc, value2Ptr);
 	    goto checkForCatch;
 	}
@@ -5137,6 +5137,7 @@ TclExecuteByteCode(interp, codePtr)
 		    goto error;
 		}
 		/* -NaN => NaN */
+		result = TCL_OK;
 		NEXT_INST_F(1, 0, 0);
 	    }
 #endif
@@ -5403,7 +5404,7 @@ TclExecuteByteCode(interp, codePtr)
 	/* Non-numeric argument... */
 	if (*pc == INST_UPLUS) {
 	    /* ... +$NonNumeric => raise an error */
-	    TRACE(("\"%.20s\" => ILLEGAL TYPE %s \n", s,
+	    TRACE(("\"%.20s\" => ILLEGAL TYPE %s \n", O2S(valuePtr),
 		    (valuePtr->typePtr? valuePtr->typePtr->name : "null")));
 	    IllegalExprOperandType(interp, pc, valuePtr);
 	    goto checkForCatch;
