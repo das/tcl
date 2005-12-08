7cfc47f658b81fdcf5c37030fe18d5802a78b888
hobbs <hobbs>
2005-12-08 20:20:11 +0000

    * generic/tcl.h: use struct __stat64 (not _stat64) for MSC_VER >= 	1400 (i.e. latest Platform SDK).

--- generic/tcl.h
+++ generic/tcl.h
@@ -396,7 +396,7 @@ typedef struct stati64 Tcl_StatBuf;
 #         if _MSC_VER < 1400
 typedef struct _stati64	Tcl_StatBuf;
 #         else
-typedef struct _stat64	Tcl_StatBuf;
+typedef struct __stat64	Tcl_StatBuf;
 #         endif /* _MSC_VER < 1400 */
 #         define TCL_LL_MODIFIER	"I64"
 #         define TCL_LL_MODIFIER_SIZE	3
