fe3b7a6000255188458df0e3cbfaa5631ca50ec0
hobbs <hobbs>
2006-05-30 00:29:37 +0000

    * generic/tcl.h (Tcl_DecrRefCount): use if/else construct to allow 	placement in unbraced outer if/else conditions. (jcw)

--- generic/tcl.h
+++ generic/tcl.h
@@ -824,8 +824,11 @@ int		Tcl_IsShared _ANSI_ARGS_((Tcl_Obj *objPtr));
 #else
 #   define Tcl_IncrRefCount(objPtr) \
 	++(objPtr)->refCount
+    /*
+     * Use empty if ; else to handle use in unbraced outer if/else conditions
+     */
 #   define Tcl_DecrRefCount(objPtr) \
-	if (--(objPtr)->refCount <= 0) TclFreeObj(objPtr)
+	if (--(objPtr)->refCount > 0) ; else TclFreeObj(objPtr)
 #   define Tcl_IsShared(objPtr) \
 	((objPtr)->refCount > 1)
 #endif
