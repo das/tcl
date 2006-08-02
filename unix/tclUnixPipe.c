717d0db728581447f5cbdb8ec97e29b01910e3bb
das <das>
2006-08-02 20:04:40 +0000

    * unix/tclUnixPipe.c (TclpCreateProcess): for USE_VFORK: ensure standard     channels are initialized before vfork() so that the child doesn't     potentially corrupt global state in the parent's address space.

--- unix/tclUnixPipe.c
+++ unix/tclUnixPipe.c
@@ -430,6 +430,23 @@ TclpCreateProcess(interp, argc, argv, inputFile, outputFile, errorFile,
 	newArgv[i] = Tcl_UtfToExternalDString(NULL, argv[i], -1, &dsArray[i]);
     }
 
+#ifdef USE_VFORK
+    /*
+     * After vfork(), do not call code in the child that changes global state,
+     * because it is using the parent's memory space at that point and writes
+     * might corrupt the parent: so ensure standard channels are initialized in
+     * the parent, otherwise SetupStdFile() might initialize them in the child.
+     */
+    if (!inputFile) {
+	Tcl_GetStdChannel(TCL_STDIN);
+    }
+    if (!outputFile) {
+        Tcl_GetStdChannel(TCL_STDOUT);
+    }
+    if (!errorFile) {
+        Tcl_GetStdChannel(TCL_STDERR);
+    }
+#endif
     pid = fork();
     if (pid == 0) {
 	int joinThisError = errorFile && (errorFile == outputFile);
