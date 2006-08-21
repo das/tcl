896e07c9087055248633d280a08bd32b2979835e
das <das>
2006-08-21 01:08:03 +0000

    * macosx/tclMacOSXNotify.c (Tcl_WaitForEvent): if the run loop is 	already running (e.g. if Tcl_WaitForEvent was called recursively), 	re-run it in a custom run loop mode containing only the source for the 	notifier thread, otherwise wakeups from other sources added to the 	common run loop modes might get lost.
    	* unix/tclUnixNotfy.c (Tcl_WaitForEvent): on 64-bit Darwin,
    	pthread_cond_timedwait() appears to have a bug that causes it to wait
    	forever when passed an absolute time which has already been exceeded by
    	the system time; as a workaround, when given a very brief timeout, just
    	do a poll on that platform. [Bug 1457797]

--- unix/tclUnixNotfy.c
+++ unix/tclUnixNotfy.c
@@ -740,7 +740,17 @@ Tcl_WaitForEvent(
     Tcl_MutexLock(&notifierMutex);
 
     waitForFiles = (tsdPtr->numFdBits > 0);
-    if (myTimePtr != NULL && myTimePtr->sec == 0 && myTimePtr->usec == 0) {
+    if (myTimePtr != NULL && myTimePtr->sec == 0 && (myTimePtr->usec == 0
+#if defined(__APPLE__) && defined(__LP64__)
+	    /*
+	     * On 64-bit Darwin, pthread_cond_timedwait() appears to have a bug
+	     * that causes it to wait forever when passed an absolute time which
+	     * has already been exceeded by the system time; as a workaround,
+	     * when given a very brief timeout, just do a poll. [Bug 1457797]
+	     */
+	    || myTimePtr->usec < 10
+#endif
+	    )) {
 	/*
 	 * Cannot emulate a polling select with a polling condition variable.
 	 * Instead, pretend to wait for files and tell the notifier thread
