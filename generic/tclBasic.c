79cba50234e6fb16b223edcac8edb11f8ad0197e
hobbs <hobbs>
2006-08-30 17:24:25 +0000

    * generic/tclBasic.c (Tcl_CreateInterp): init iPtr->threadId

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -363,6 +363,7 @@ Tcl_CreateInterp()
     iPtr->emptyObjPtr = Tcl_NewObj(); /* another empty object */
     Tcl_IncrRefCount(iPtr->emptyObjPtr);
     iPtr->resultSpace[0] = 0;
+    iPtr->threadId = Tcl_GetCurrentThread();
 
     iPtr->globalNsPtr = NULL;	/* force creation of global ns below */
     iPtr->globalNsPtr = (Namespace *) Tcl_CreateNamespace(interp, "",
