f0406ddb66bbdb38abcb6fe9402bf5aa6fb58cc5
dkf <dkf>
2006-10-20 14:04:00 +0000

    Consolidated TIP#257 patch applied to HEAD to allow for experimentation by other developers

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -526,11 +526,16 @@ Tcl_CreateInterp(void)
 #endif
 
     /*
-     * TIP #59: Make embedded configuration information
-     * available.
+     * TIP #59: Make embedded configuration information available.
      */
 
     TclInitEmbeddedConfigurationInformation(interp);
+
+    /*
+     * TIP #257: Install the OO engine (for testing).
+     */
+
+    TclOOInit(interp);
 
     /*
      * Compute the byte order of this machine.
@@ -1938,8 +1943,8 @@ TclInvokeObjectCommand(
 int
 TclRenameCommand(
     Tcl_Interp *interp,		/* Current interpreter. */
-    char *oldName,		/* Existing command name. */
-    char *newName)		/* New command name. */
+    const char *oldName,	/* Existing command name. */
+    const char *newName)	/* New command name. */
 {
     Interp *iPtr = (Interp *) interp;
     CONST char *newTail;
@@ -1956,8 +1961,7 @@ TclRenameCommand(
      * found.
      */
 
-    cmd = Tcl_FindCommand(interp, oldName, NULL,
-	    /*flags*/ 0);
+    cmd = Tcl_FindCommand(interp, oldName, NULL, /*flags*/ 0);
     cmdPtr = (Command *) cmd;
     if (cmdPtr == NULL) {
 	Tcl_AppendResult(interp, "can't ",
--- generic/tclNamesp.c
+++ generic/tclNamesp.c
@@ -117,8 +117,8 @@ typedef struct EnsembleConfig {
 				 * all lists, and cannot be found by scanning
 				 * the list from the namespace's ensemble
 				 * field. */
-    int flags;			/* ORed combo of ENS_DEAD and
-				 * TCL_ENSEMBLE_PREFIX. */
+    int flags;			/* ORed combo of TCL_ENSEMBLE_PREFIX and
+				 * ENS_DEAD. */
 
     /* OBJECT FIELDS FOR ENSEMBLE CONFIGURATION */
 
@@ -251,8 +251,6 @@ static void		FreeEnsembleCmdRep(Tcl_Obj *objPtr);
 static void		DupEnsembleCmdRep(Tcl_Obj *objPtr, Tcl_Obj *copyPtr);
 static void		StringOfEnsembleCmdRep(Tcl_Obj *objPtr);
 static void		UnlinkNsPath(Namespace *nsPtr);
-static void		SetNsPath(Namespace *nsPtr, int pathLength,
-			    Tcl_Namespace *pathAry[]);
 
 /*
  * This structure defines a Tcl object type that contains a namespace
@@ -4110,7 +4108,7 @@ NamespacePathCmd(
      * Now we have the list of valid namespaces, install it as the path.
      */
 
-    SetNsPath(nsPtr, nsObjc, namespaceList);
+    TclSetNsPath(nsPtr, nsObjc, namespaceList);
 
     result = TCL_OK;
   badNamespace:
@@ -4123,7 +4121,7 @@ NamespacePathCmd(
 /*
  *----------------------------------------------------------------------
  *
- * SetNsPath --
+ * TclSetNsPath --
  *
  *	Sets the namespace command name resolution path to the given list of
  *	namespaces. If the list is empty (of zero length) the path is set to
@@ -4141,8 +4139,8 @@ NamespacePathCmd(
  */
 
 /* EXPOSE ME? */
-static void
-SetNsPath(
+void
+TclSetNsPath(
     Namespace *nsPtr,		/* Namespace whose path is to be set. */
     int pathLength,		/* Length of pathAry */
     Tcl_Namespace *pathAry[])	/* Array of namespaces that are the path. */
