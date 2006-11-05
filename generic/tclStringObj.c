1bc1f92e7bfdd99c07ee467399be18a51cee0b42
dgp <dgp>
2006-11-05 04:16:07 +0000

    * generic/tclStringObj.c:       Changed Tcl_ObjPrintf() response to         an invalid format specifier string.  No longer panics; now produces         an error message as output.

--- generic/tclStringObj.c
+++ generic/tclStringObj.c
@@ -2456,7 +2456,8 @@ AppendPrintfToObjVA(
     Tcl_ListObjGetElements(NULL, list, &objc, &objv);
     code = TclAppendFormatToObj(NULL, objPtr, format, objc, objv);
     if (code != TCL_OK) {
-	Tcl_Panic("Unable to format \"%s\" with supplied arguments: %s",
+	TclAppendPrintfToObj(objPtr,
+		"Unable to format \"%s\" with supplied arguments: %s",
 		format, Tcl_GetString(list));
     }
     Tcl_DecrRefCount(list);
