6a3375035f6eac11201b52af1b35a7619f5ff0c5
dgp <dgp>
2006-12-08 20:48:08 +0000

    * generic/tclBasic.c:    Another step down the path of re-using         * generic/tclCompExpr.c: TclExecuteByteCode to implement the TIP 174         * generic/tclCompile.h: commands instead of using a mass of code         * generic/tclMathOp.c:  duplication.  Now all operator commands that         * tests/mathop.test:    demand exactly one operation are implemented         via TclSingleOpCmd and a call to TEBC.

--- generic/tclCompile.h
+++ generic/tclCompile.h
@@ -746,6 +746,16 @@ typedef struct JumptableInfo {
 } JumptableInfo;
 
 MODULE_SCOPE AuxDataType	tclJumptableInfoType;
+
+/*
+ * ClientData type used by the math operator commands.
+ */
+typedef struct {
+    const char *operator;
+    const char *expected;
+    int numArgs;
+} TclOpCmdClientData;
+
 
 /*
  *----------------------------------------------------------------
@@ -845,6 +855,9 @@ MODULE_SCOPE int	TclRegisterLiteral(CompileEnv *envPtr,
 MODULE_SCOPE void	TclReleaseLiteral(Tcl_Interp *interp, Tcl_Obj *objPtr);
 MODULE_SCOPE void	TclSetCmdNameObj(Tcl_Interp *interp, Tcl_Obj *objPtr,
 			    Command *cmdPtr);
+MODULE_SCOPE int	TclSingleOpCmd(ClientData clientData,
+			    Tcl_Interp *interp, int objc,
+			    Tcl_Obj *CONST objv[]);
 #ifdef TCL_COMPILE_DEBUG
 MODULE_SCOPE void	TclVerifyGlobalLiteralTable(Interp *iPtr);
 MODULE_SCOPE void	TclVerifyLocalLiteralTable(CompileEnv *envPtr);
