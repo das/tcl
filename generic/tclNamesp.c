adcbe00afd719a631cc41a5544e49053ba3f2c7b
dgp <dgp>
2007-02-08 18:43:39 +0000

    * generic/tclNamesp.c:  Corrected broken logic in Tcl_DeleteNamespace()         * tests/namespace.test: introduced in Patch 1577278 that caused         [namespace delete ::] to be effective only at level #0.  New test         namespace-7.7 should prevent similar error in the future. [Bug 1655305]

--- generic/tclNamesp.c
+++ generic/tclNamesp.c
@@ -505,7 +505,8 @@ Tcl_PopCallFrame(
 
     nsPtr = framePtr->nsPtr;
     nsPtr->activationCount--;
-    if ((nsPtr->flags & NS_DYING) && (nsPtr->activationCount == 0)) {
+    if ((nsPtr->flags & NS_DYING)
+	    && (nsPtr->activationCount - (nsPtr == iPtr->globalNsPtr) == 0)) {
 	Tcl_DeleteNamespace((Tcl_Namespace *) nsPtr);
     }
     framePtr->nsPtr = NULL;
@@ -967,8 +968,7 @@ Tcl_DeleteNamespace(
      * refCount reaches 0.
      */
 
-    if ((nsPtr->activationCount > 0)
-	    && !((nsPtr == globalNsPtr) && (nsPtr->activationCount == 1))) {
+    if (nsPtr->activationCount - (nsPtr == globalNsPtr) > 0) {
 	nsPtr->flags |= NS_DYING;
 	if (nsPtr->parentPtr != NULL) {
 	    entryPtr = Tcl_FindHashEntry(&nsPtr->parentPtr->childTable,
