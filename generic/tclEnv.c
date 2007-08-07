4bcd055fd9324079c22f02568a80113a80b8e6b4
das <das>
2007-08-07 05:04:48 +0000

    * generic/tclEnv.c:	improve environ handling on Mac OS X (adapted 	* unix/tclUnixPort.h:	from Apple changes in Darwin tcl-64).

--- generic/tclEnv.c
+++ generic/tclEnv.c
@@ -39,15 +39,6 @@ static int environSize = 0;	/* Non-zero means that the environ array was
 #endif
 
 /*
- * For MacOS X
- */
-#if defined(__APPLE__) && defined(__DYNAMIC__)
-#include <crt_externs.h>
-__private_extern__ char **environ;
-char **environ = NULL;
-#endif
-
-/*
  * Declarations for local procedures defined in this file:
  */
 
@@ -95,13 +86,6 @@ TclSetupEnv(interp)
     Tcl_DString envString;
     char *p1, *p2;
     int i;
-
-    /*
-     * For MacOS X
-     */
-#if defined(__APPLE__) && defined(__DYNAMIC__)
-    environ = *_NSGetEnviron();
-#endif
 
     /*
      * Synchronize the values in the environ array with the contents
@@ -214,12 +198,6 @@ TclSetEnv(name, value)
 	    }
 	    environ = ourEnviron = newEnviron;
 	    environSize = length + 5;
-#if defined(__APPLE__) && defined(__DYNAMIC__)
-	    {
-	    char ***e = _NSGetEnviron();
-	    *e = environ;
-	    }
-#endif
 	}
 	index = length;
 	environ[index + 1] = NULL;
