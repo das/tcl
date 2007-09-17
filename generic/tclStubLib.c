f405b745c8c1ee856ee3c026d321a4bd47c26f89
dgp <dgp>
2007-09-17 15:10:50 +0000

    merge updates from HEAD

--- generic/tclStubLib.c
+++ generic/tclStubLib.c
@@ -95,9 +95,27 @@ Tcl_InitStubs(
 	return NULL;
     }
 
-    actualVersion = Tcl_PkgRequireEx(interp, "Tcl", version, exact, &pkgData);
+    actualVersion = Tcl_PkgRequireEx(interp, "Tcl", version, 0, &pkgData);
     if (actualVersion == NULL) {
 	return NULL;
+    }
+    if (exact) {
+	CONST char *p = version;
+	int count = 0;
+
+	while (*p) {
+	    count += !isdigit(*p++);
+	}
+	if (count == 1) {
+	    if (0 != strncmp(version, actualVersion, strlen(version))) {
+		return NULL;
+	    }
+	} else {
+	    actualVersion = Tcl_PkgRequireEx(interp, "Tcl", version, 1, NULL);
+	    if (actualVersion == NULL) {
+		return NULL;
+	    }
+	}
     }
     tclStubsPtr = (TclStubs*)pkgData;
 
