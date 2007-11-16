d7a01606a9a7eeab691f3a850af31ed6adcc0d38
dgp <dgp>
2007-11-16 07:20:49 +0000

    merge updates from HEAD

--- unix/tclUnixInit.c
+++ unix/tclUnixInit.c
@@ -38,11 +38,9 @@
 #endif
 
 /*
- * Define this if you want to revert to the old behavior of never checking the
- * stack.
+ * Define TCL_NO_STACK_CHECK in the compiler options if you want to revert to
+ * the old behavior of never checking the stack.
  */
-
-#undef TCL_NO_STACK_CHECK
 
 /*
  * Define this if you want to see a lot of output regarding stack checking.
