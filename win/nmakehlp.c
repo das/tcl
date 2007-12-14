ccbbf35b10bbc4876e4500240c97a6989396ea3a
patthoyts <patthoyts>
2007-12-14 02:27:07 +0000

    Support compilation with MSVC9 on AMD64

--- win/nmakehlp.c
+++ win/nmakehlp.c
@@ -21,8 +21,14 @@
 #pragma comment (lib, "kernel32.lib")
 #include <stdio.h>
 #include <math.h>
+
+/*
+ * This library is required for x64 builds with _some_ versions
+ */
 #if defined(_M_IA64) || defined(_M_AMD64)
+#if _MSC_FULL_VER > 140000000 && _MSC_FULL_VER <= 140040310
 #pragma comment(lib, "bufferoverflowU")
+#endif
 #endif
 
 /* ISO hack for dumb VC++ */
