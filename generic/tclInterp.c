6f2937664225768055dccf011a310ec384420885
dgp <dgp>
2008-06-20 19:23:23 +0000

    * generic/tclInterp.c:  Fixed completely boneheaded mistake that         * tests/interp.test:    [interp bgerror $slave] and [$slave bgerror]         would always act like [interp bgerror {}].  [Bug 1999035].
            * tests/chanio.test:    Corrected flawed tests revealed by a -debug 1
            * tests/event.test:     -singleproc 1 test suite run.
            * tests/io.test:

--- generic/tclInterp.c
+++ generic/tclInterp.c
@@ -2061,9 +2061,9 @@ SlaveBgerror(
 		    NULL);
 	    return TCL_ERROR;
 	}
-	TclSetBgErrorHandler(interp, objv[0]);
+	TclSetBgErrorHandler(slaveInterp, objv[0]);
     }
-    Tcl_SetObjResult(interp, TclGetBgErrorHandler(interp));
+    Tcl_SetObjResult(interp, TclGetBgErrorHandler(slaveInterp));
     return TCL_OK;
 }
 
