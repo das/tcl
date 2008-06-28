ce237d4018f1ea98779aa682e3fc69f1c93d86d6
dgp <dgp>
2008-06-28 04:22:04 +0000

    * generic/tclPathObj.c:  Plug memory leak in [Bug 1972879] fix.  Thanks         Rolf Ade for detecting and Dan Steffen for the fix [Bug 2004654].

--- generic/tclPathObj.c
+++ generic/tclPathObj.c
@@ -1602,10 +1602,15 @@ Tcl_FSGetTranslatedPath(
 	     * translated version of cwdPtr to normPathPtr, we'll get the
 	     * translated result we need, and can store it for future use.
 	     */
-	    retObj = Tcl_FSJoinToPath(Tcl_FSGetTranslatedPath(interp,
-		    srcFsPathPtr->cwdPtr), 1, &(srcFsPathPtr->normPathPtr));
+
+	    Tcl_Obj *translatedCwdPtr = Tcl_FSGetTranslatedPath(interp,
+		    srcFsPathPtr->cwdPtr);
+
+	    retObj = Tcl_FSJoinToPath(translatedCwdPtr, 1,
+		    &(srcFsPathPtr->normPathPtr));
 	    srcFsPathPtr->translatedPathPtr = retObj;
 	    Tcl_IncrRefCount(retObj);
+	    Tcl_DecrRefCount(translatedCwdPtr);
 	} else {
 	    /*
 	     * It is a pure absolute, normalized path object. This is
