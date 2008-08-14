b727105e594fd3797b35fd0b61e944c33d21033d
dgp <dgp>
2008-08-14 15:16:11 +0000

    merge updates from HEAD

--- generic/tclDTrace.d
+++ generic/tclDTrace.d
@@ -63,9 +63,11 @@ provider tcl {
      *		arg3: TIP 280 file			(string)
      *		arg4: TIP 280 line			(int)
      *		arg5: TIP 280 level			(int)
+     *		arg6: TclOO method			(string)
+     *		arg7: TclOO class/object		(string)
      */
     probe proc__info(char* cmd, char* type, char* proc, char* file, int line,
-	    int level);
+	    int level, char* method, char* class);
 
     /***************************** cmd probes ******************************/
     /*
@@ -112,9 +114,11 @@ provider tcl {
      *		arg3: TIP 280 file			(string)
      *		arg4: TIP 280 line			(int)
      *		arg5: TIP 280 level			(int)
+     *		arg6: TclOO method			(string)
+     *		arg7: TclOO class/object		(string)
      */
     probe cmd__info(char* cmd, char* type, char* proc, char* file, int line,
-	    int level);
+	    int level, char* method, char* class);
 
     /***************************** inst probes *****************************/
     /*
