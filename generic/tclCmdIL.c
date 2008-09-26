0294bb38d98e96cf6dec3293cd335d7beb01321c
dgp <dgp>
2008-09-26 21:05:54 +0000

    TIP #323 IMPLEMENTATION (partial)
            * doc/lrepeat.n:        Revise [lrepeat] to accept both zero
            * generic/tclCmdIL.c:   repetitions and zero elements to be repeated.
            * tests/lrepeat.test:

--- generic/tclCmdIL.c
+++ generic/tclCmdIL.c
@@ -2443,25 +2443,25 @@ Tcl_LrepeatObjCmd(
     register Tcl_Obj *const objv[])
 				/* The argument objects. */
 {
-    int elementCount, i, result;
+    int elementCount, i;
     Tcl_Obj *listPtr, **dataArray;
     List *listRepPtr;
 
     /*
      * Check arguments for legality:
-     *		lrepeat posInt value ?value ...?
+     *		lrepeat count ?value ...?
      */
 
-    if (objc < 3) {
-	Tcl_WrongNumArgs(interp, 1, objv, "positiveCount value ?value ...?");
+    if (objc < 2) {
+	Tcl_WrongNumArgs(interp, 1, objv, "count ?value ...?");
 	return TCL_ERROR;
     }
-    result = TclGetIntFromObj(interp, objv[1], &elementCount);
-    if (result == TCL_ERROR) {
+    if (TCL_OK != TclGetIntFromObj(interp, objv[1], &elementCount)) {
 	return TCL_ERROR;
     }
-    if (elementCount < 1) {
-	Tcl_AppendResult(interp, "must have a count of at least 1", NULL);
+    if (elementCount < 0) {
+	Tcl_SetObjResult(interp, Tcl_Format(NULL,
+		"bad count \"%d\": must be integer >= 0", 1, objv+1));
 	return TCL_ERROR;
     }
 
