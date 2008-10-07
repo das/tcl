246688650d6daa0aefbe909d7d18abdc56a70e64
das <das>
2008-10-07 01:15:34 +0000

    fix warnings from 2008-10-05 constification

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -391,19 +391,21 @@ VarHashCreateVar(
 #define TCL_DTRACE_INST_NEXT() \
     if (TCL_DTRACE_INST_DONE_ENABLED()) {\
 	if (curInstName) {\
-	    TCL_DTRACE_INST_DONE(curInstName, (int) CURR_DEPTH, tosPtr);\
+	    TCL_DTRACE_INST_DONE((char* ) curInstName, (int) CURR_DEPTH,\
+		    tosPtr);\
 	}\
 	curInstName = tclInstructionTable[*pc].name;\
 	if (TCL_DTRACE_INST_START_ENABLED()) {\
-	    TCL_DTRACE_INST_START(curInstName, (int) CURR_DEPTH, tosPtr);\
+	    TCL_DTRACE_INST_START((char*) curInstName, (int) CURR_DEPTH,\
+		    tosPtr);\
 	}\
     } else if (TCL_DTRACE_INST_START_ENABLED()) {\
-	TCL_DTRACE_INST_START(tclInstructionTable[*pc].name, (int) CURR_DEPTH,\
-		tosPtr);\
+	TCL_DTRACE_INST_START((char*) tclInstructionTable[*pc].name,\
+		(int) CURR_DEPTH, tosPtr);\
     }
 #define TCL_DTRACE_INST_LAST() \
     if (TCL_DTRACE_INST_DONE_ENABLED() && curInstName) {\
-	TCL_DTRACE_INST_DONE(curInstName, (int) CURR_DEPTH, tosPtr);\
+	TCL_DTRACE_INST_DONE((char*) curInstName, (int) CURR_DEPTH, tosPtr);\
     }
 
 /*
