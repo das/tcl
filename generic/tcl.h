c22c13dfa69db35e66ccee95ef3afac61af5b2ea
dgp <dgp>
2008-10-10 18:16:47 +0000

    * generic/tcl.h:        Bump to 8.5.5 for release.         * library/init.tcl:         * tools/tcl.wse.in:         * unix/configure.in:         * unix/tcl.spec:         * win/configure.in:
            * unix/configure:       autoconf-2.59
            * win/configure:

--- generic/tcl.h
+++ generic/tcl.h
@@ -63,7 +63,7 @@ extern "C" {
 #define TCL_RELEASE_SERIAL  5
 
 #define TCL_VERSION	    "8.5"
-#define TCL_PATCH_LEVEL	    "8.5.5b1"
+#define TCL_PATCH_LEVEL	    "8.5.5"
 
 /*
  * The following definitions set up the proper options for Windows compilers.
