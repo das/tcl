c6918038df31bcbd7bc925cd83632479473c2f4a
ferrieux <ferrieux>
2008-12-16 22:04:00 +0000

    Backport re-fix of [2431847]

--- generic/tclExecute.c
+++ generic/tclExecute.c
@@ -6678,10 +6678,7 @@ TclExecuteByteCode(
 		    "%u => ERROR reading leaf dictionary key \"%s\": ",
 		    opnd, O2S(dictPtr)), Tcl_GetObjResult(interp));
 	} else {
-	    Tcl_Obj *tmpObj;
-
-	    TclNewObj(tmpObj);
-	    Tcl_SetObjResult(interp, tmpObj);
+	    Tcl_ResetResult(interp);
 	    Tcl_AppendResult(interp, "key \"", TclGetString(OBJ_AT_TOS),
 		    "\" not known in dictionary", NULL);
 	    TRACE_WITH_OBJ(("%u => ERROR ", opnd), Tcl_GetObjResult(interp));
