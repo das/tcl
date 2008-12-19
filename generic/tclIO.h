4b72e3dbfdd373269133fcb46fe9096444b1fc41
dgp <dgp>
2008-12-19 03:37:12 +0000

    merge updates from HEAD

--- generic/tclIO.h
+++ generic/tclIO.h
@@ -339,6 +339,9 @@ typedef struct ChannelState {
 					 * Used by Channel Tcl_Obj type to
 					 * determine if we have to revalidate
 					 * the channel. */
+#define CHANNEL_CLOSEDWRITE	(1<<21)	/* Channel write side has been closed.
+					 * No further Tcl-level write IO on
+					 * the channel is allowed. */
 
 /*
  * For each channel handler registered in a call to Tcl_CreateChannelHandler,
