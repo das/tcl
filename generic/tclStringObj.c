0ae6cda5b02ec6358fd6bb5276605df2bf2d2835
dgp <dgp>
2009-02-02 14:24:23 +0000

    merge updates from HEAD

--- generic/tclStringObj.c
+++ generic/tclStringObj.c
@@ -110,7 +110,8 @@ typedef struct String {
 #define STRING_SIZE(numBytes) \
 	(sizeof(String) - sizeof(Tcl_UniChar) + (numBytes))
 #define STRING_NOMEM(numBytes) \
-	(Tcl_Panic("unable to alloc %u bytes", STRING_SIZE(numBytes)), NULL)
+	(Tcl_Panic("unable to alloc %u bytes", STRING_SIZE(numBytes)), \
+	 (char *) NULL)
 #define stringAlloc(numBytes) \
 	(String *) (((numBytes) > INT_MAX - STRING_SIZE(0)) \
 	    ? STRING_NOMEM(numBytes) \
@@ -763,6 +764,14 @@ Tcl_SetObjLength(
 {
     String *stringPtr;
 
+    if (length < 0) {
+	/*
+	 * Setting to a negative length is nonsense.  This is probably the
+	 * result of overflowing the signed integer range.
+	 */
+	Tcl_Panic(	"Tcl_SetObjLength: negative length requested: "
+			"%d (integer overflow?)", length);
+    }
     if (Tcl_IsShared(objPtr)) {
 	Tcl_Panic("%s called with shared object", "Tcl_SetObjLength");
     }
@@ -876,6 +885,13 @@ Tcl_AttemptSetObjLength(
 {
     String *stringPtr;
 
+    if (length < 0) {
+	/*
+	 * Setting to a negative length is nonsense.  This is probably the
+	 * result of overflowing the signed integer range.
+	 */
+	return 0;
+    }
     if (Tcl_IsShared(objPtr)) {
 	Tcl_Panic("%s called with shared object", "Tcl_AttemptSetObjLength");
     }
