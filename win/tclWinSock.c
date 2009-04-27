7b8cf635386537885ddb2e09c7872a07cac477b2
ferrieux <ferrieux>
2009-04-27 21:25:17 +0000

    Backport fix for [Bug 2446662]: resync Win behavior on RST with that of unix (EOF).

--- win/tclWinSock.c
+++ win/tclWinSock.c
@@ -1769,11 +1769,23 @@ TcpInputProc(instanceData, buf, toRead, errorCodePtr)
 	    break;
 	}
   
+ 	error = winSock.WSAGetLastError();
+ 
+ 	/*
+ 	 * If an RST comes, then ignore the error and report an EOF just like
+ 	 * on unix.
+ 	 */
+ 
+ 	if (error == WSAECONNRESET) {
+ 	    infoPtr->flags |= SOCKET_EOF;
+ 	    bytesRead = 0;
+ 	    break;
+ 	}
+ 
 	/*
 	 * Check for error condition or underflow in non-blocking case.
 	 */
   
-	error = winSock.WSAGetLastError();
 	if ((infoPtr->flags & SOCKET_ASYNC) || (error != WSAEWOULDBLOCK)) {
 	    TclWinConvertWSAError(error);
 	    *errorCodePtr = Tcl_GetErrno();
