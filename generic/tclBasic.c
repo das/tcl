28f8c94a1f5c5c557874558c893d13b78f23b828
dkf <dkf>
2009-08-02 13:03:46 +0000

    Added *unsupported* command to report an object's representation.

--- generic/tclBasic.c
+++ generic/tclBasic.c
@@ -782,11 +782,13 @@ Tcl_CreateInterp(void)
 	    TclDefaultBgErrorHandlerObjCmd, NULL, NULL);
 
     /*
-     * Create an unsupported command for debugging bytecode.
+     * Create unsupported commands for debugging bytecode and objects.
      */
 
     Tcl_CreateObjCommand(interp, "::tcl::unsupported::disassemble",
 	    Tcl_DisassembleObjCmd, NULL, NULL);
+    Tcl_CreateObjCommand(interp, "::tcl::unsupported::representation",
+	    Tcl_RepresentationCmd, NULL, NULL);
 
     /*
      * Create the 'tailcall' command
@@ -7863,7 +7865,7 @@ DTraceCmdReturn(
     return result;
 }
 
-TCL_DTRACE_DEBUG_LOG();
+TCL_DTRACE_DEBUG_LOG()
 
 #endif /* USE_DTRACE */
 
--- generic/tclInt.h
+++ generic/tclInt.h
@@ -3138,6 +3138,9 @@ MODULE_SCOPE int	Tcl_RegsubObjCmd(ClientData clientData,
 MODULE_SCOPE int	Tcl_RenameObjCmd(ClientData clientData,
 			    Tcl_Interp *interp, int objc,
 			    Tcl_Obj *const objv[]);
+MODULE_SCOPE int	Tcl_RepresentationCmd(ClientData clientData,
+			    Tcl_Interp *interp, int objc,
+			    Tcl_Obj *const objv[]);
 MODULE_SCOPE int	Tcl_ReturnObjCmd(ClientData clientData,
 			    Tcl_Interp *interp, int objc,
 			    Tcl_Obj *const objv[]);
