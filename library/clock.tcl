870f5ff2bd4d6d5e116a9cd9a01ae2d14403bd72
kennykb <kennykb>
2009-10-27 03:23:32 +0000

    * library/clock.tcl (ParseClockScanFormat): Corrected a problem where [clock scan] didn't load the timezone soon enough when processing a time format that lacked a complete date. [Bug 2886852] * tests/clock.test (clock-66.1): Added a test case for the above bug.

--- library/clock.tcl
+++ library/clock.tcl
@@ -1955,6 +1955,21 @@ proc ::tcl::clock::ParseClockScanFormat {formatString locale} {
     append procBody $postcode
     append procBody [list set changeover [mc GREGORIAN_CHANGE_DATE]] \n
 
+    # Set up the time zone before doing anything with a default base date
+    # that might need a timezone to interpret it.
+
+    if { ![dict exists $fieldSet seconds] 
+	    && ![dict exists $fieldSet starDate] } {
+	if { [dict exists $fieldSet tzName] } {
+	    append procBody {
+		set timeZone [dict get $date tzName]
+	    }
+	}
+	append procBody {
+	    ::tcl::clock::SetupTimeZone $timeZone
+	}
+    }
+
     # Add code that gets Julian Day Number from the fields.
 
     append procBody [MakeParseCodeFromFields $fieldSet $DateParseActions]
@@ -1963,7 +1978,9 @@ proc ::tcl::clock::ParseClockScanFormat {formatString locale} {
 
     append procBody [MakeParseCodeFromFields $fieldSet $TimeParseActions]
 
-    # Assemble seconds, and convert local nominal time to UTC.
+    # Assemble seconds from the Julian day and second of the day.
+    # Convert to local time unless epoch seconds or stardate are
+    # being processed - they're always absolute
 
     if { ![dict exists $fieldSet seconds] 
          && ![dict exists $fieldSet starDate] } {
@@ -1978,17 +1995,10 @@ proc ::tcl::clock::ParseClockScanFormat {formatString locale} {
 		+ [dict get $date secondOfDay]
 	    }]
 	}
-    }
 
-    if { ![dict exists $fieldSet seconds] 
-	    && ![dict exists $fieldSet starDate] } {
-	if { [dict exists $fieldSet tzName] } {
-	    append procBody {
-		set timeZone [dict get $date tzName]
-	    }
-	}
+	# Finally, convert the date to local time
+
 	append procBody {
-	    ::tcl::clock::SetupTimeZone $timeZone
 	    set date [::tcl::clock::ConvertLocalToUTC $date[set date {}] \
 			  $TZData($timeZone) $changeover]
 	}
