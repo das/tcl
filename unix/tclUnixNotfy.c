cf233529fb5d38d8fc93a2ed8d2b1143385fd9b0
nijtmans <nijtmans>
2009-12-17 00:06:21 +0000

    Fix gcc warning: ignoring return value of ‘write’, declared with attribute warn_unused_result

--- unix/tclUnixNotfy.c
+++ unix/tclUnixNotfy.c
@@ -785,7 +785,7 @@ Tcl_WaitForEvent(
 	    tsdPtr->onList = 1;
 
 	    if (write(triggerPipe, "", 1) != 1) {
-			Tcl_Panic("Tcl_FinalizeNotifier: unable to write to triggerPipe");
+			Tcl_Panic("Tcl_WaitForEvent: unable to write to triggerPipe");
 	    }
 	}
 
@@ -817,7 +817,7 @@ Tcl_WaitForEvent(
 	    tsdPtr->nextPtr = tsdPtr->prevPtr = NULL;
 	    tsdPtr->onList = 0;
 	    if (write(triggerPipe, "", 1) != 1) {
-			Tcl_Panic("Tcl_FinalizeNotifier: unable to write to triggerPipe");
+			Tcl_Panic("Tcl_WaitForEvent: unable to write to triggerPipe");
 	    }
 	}
 
