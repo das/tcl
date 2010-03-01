b76c5f82da500a163ed0bc900563153e5de80406
ferrieux <ferrieux>
2010-03-01 23:19:36 +0000

    Improve error message for corner case in ::prefix

--- generic/tclIndexObj.c
+++ generic/tclIndexObj.c
@@ -367,7 +367,7 @@ Tcl_GetIndexFromObjStruct(
 	Tcl_AppendStringsToObj(resultPtr, (numAbbrev > 1) &&
 			       !(flags & TCL_EXACT) ? "ambiguous " : "bad ", msg, " \"", key, NULL);
 	if (STRING_AT(tablePtr, offset, 0) == NULL) {
-	    Tcl_AppendStringsToObj(resultPtr, "\": empty table !", NULL);
+	    Tcl_AppendStringsToObj(resultPtr, "\": no valid options", NULL);
 	} else {
 	    Tcl_AppendStringsToObj(resultPtr, "\": must be ", STRING_AT(tablePtr, offset, 0), NULL);
 	    for (entryPtr = NEXT_ENTRY(tablePtr, offset), count = 0;
