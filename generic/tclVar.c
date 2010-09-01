d757b7cd610d6133fc1d13e949b2423b22801869
andreas_kupries <andreas_kupries>
2010-09-01 20:35:32 +0000

    * generic/tclExecute.c: [Bug 3057639]. Applied patch by Jeff to 	* generic/tclVar.c: make the behaviour of lappend in bytecompiled 	* tests/append.test: mode consistent with direct-eval and 'append' 	* tests/appendComp.test: generally. Added tests (append*-9.*) 	showing the difference.

--- generic/tclVar.c
+++ generic/tclVar.c
@@ -1867,8 +1867,10 @@ TclPtrSetVar(
 
     /*
      * Invoke any read traces that have been set for the variable if it is
-     * requested; this is only done in the core by the INST_LAPPEND_*
-     * instructions.
+     * requested. This was done for INST_LAPPEND_* but that was inconsistent
+     * with the non-bc instruction, and would cause failures trying to
+     * lappend to any non-existing ::env var, which is inconsistent with
+     * documented behavior. [Bug #3057639].
      */
 
     if ((flags & TCL_TRACE_READS) && ((varPtr->flags & VAR_TRACED_READ)
