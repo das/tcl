400d3b8f10c4c107ca3dd69d008fba78a7184c87
kennykb <kennykb>
2010-11-04 23:56:06 +0000

    * generic/tclMain.c: Added missing conditional on _WIN32 around code that messes around with the definition of _UNICODE, to correct a badly broken Unix build from Jan's last commit.

--- generic/tclMain.c
+++ generic/tclMain.c
@@ -18,7 +18,7 @@
  * TCL_ASCII_MAIN defined. This way both Tcl_Main and Tcl_MainExW
  * can be implemented, sharing the same source code.
  */
-#ifndef TCL_ASCII_MAIN
+#if defined(_WIN32) && !defined(TCL_ASCII_MAIN)
 #   ifdef UNICODE
 #	undef UNICODE
 #	undef _UNICODE
